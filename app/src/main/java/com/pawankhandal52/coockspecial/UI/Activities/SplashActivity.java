fdea96062f3739c49a72b1ceb013abf7a3f5ab30
Pawan <pksharama@stemdot.com>
2018-11-02 15:10:59 +0530

    Initialize repo

---
+++ app/src/main/java/com/pawankhandal52/coockspecial/UI/Activities/SplashActivity.java
@@ -0,0 +1,15 @@
+package com.pawankhandal52.coockspecial.UI.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.pawankhandal52.coockspecial.R;
+
+public class SplashActivity extends AppCompatActivity {
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
